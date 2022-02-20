1a9aa0c01ac9720d2b74dadb3072fb5320ca8b6f
MeetUkani <96523875+MeetUkani@users.noreply.github.com>
2022-02-20 17:00:57 +0530

    Data Added

--- app/src/main/java/com/example/attendance_manager_try/Login_Activity.java
+++ app/src/main/java/com/example/attendance_manager_try/Login_Activity.java
@@ -4,10 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,9 +19,9 @@ public class Login_Activity extends AppCompatActivity {
     private EditText entered_Username;
     private EditText entered_Password;
     private Button login;
+    private Login_Modal login_Modal;
     DatabaseReference databaseReference;
     FirebaseDatabase firebaseDatabase;
-    private Login_Modal login_Modal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,37 +32,36 @@ public class Login_Activity extends AppCompatActivity {
         entered_Password = findViewById(R.id.password);
         login = findViewById(R.id.login);
 
-        login.setOnClickListener(view -> {
+        login.setOnClickListener(view -> { // Login Listener
             String username = entered_Username.getText().toString();
             String password = entered_Password.getText().toString();
-
+            if(username.equals("") || password.equals("")){
+                Toast.makeText(this, "Please Fill All the Feilds", Toast.LENGTH_SHORT).show();
+            }
             login_Modal = new Login_Modal(username,password);
 
             firebaseDatabase  = FirebaseDatabase.getInstance();
-            databaseReference = firebaseDatabase.getReference().child("login_credentials").child(username);
-            addData(login_Modal);
+            databaseReference = firebaseDatabase.getReference().child("login_credentials");
+            Boolean is_Correct = authenticate_Data(login_Modal);
         });
     }
 
-    private void addData(Login_Modal login_Modal) {
-        try {
-
-            databaseReference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    databaseReference.setValue(login_Modal);
-                    Toast.makeText(Login_Activity.this, "Data Entry Successful", Toast.LENGTH_SHORT).show();
+    private Boolean authenticate_Data(Login_Modal login_modal) {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snap: snapshot.getChildren()) {
+                    if()
                 }
+            }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Toast.makeText(Login_Activity.this, "Failed", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }catch (Exception e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+        return false;
     }
+
 
 }
