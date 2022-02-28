17c0861fc112b3c57f59f1f48913391986ae6c4c
MeetUkani <96523875+MeetUkani@users.noreply.github.com>
2022-02-28 17:37:52 +0530

    New activity_session_create.xml

---
+++ app/src/main/java/com/example/attendance_manager_try/Session_Create.java
@@ -0,0 +1,14 @@
+package com.example.attendance_manager_try;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Session_Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_create);
+    }
+}
