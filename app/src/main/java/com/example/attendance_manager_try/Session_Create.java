70713c6fdc9cf626a1cd93f0c11a0e5109c821fa
MeetUkani <96523875+MeetUkani@users.noreply.github.com>
2022-03-01 15:27:05 +0530

    Date Time Picker Set

--- app/src/main/java/com/example/attendance_manager_try/Session_Create.java
+++ app/src/main/java/com/example/attendance_manager_try/Session_Create.java
@@ -2,13 +2,63 @@ package com.example.attendance_manager_try;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class Session_Create extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Session_Create extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    Button time_Set;
+    TextView time_Show;
+
+    int day,month,year,hour,minute;
+    int final_day,final_month,final_year,final_hour,final_minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_create);
+
+        time_Set = findViewById(R.id.TimeSetter);
+        time_Show = findViewById(R.id.time_Shower);
+        time_Set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                year = cal.get(Calendar.YEAR);
+                month = cal.get(Calendar.MONTH);
+                day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Session_Create.this,Session_Create.this,year,month,day);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        final_year = i;
+        final_month = i1;
+        final_day = i2;
+
+        Calendar cal = Calendar.getInstance();
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(Session_Create.this,Session_Create.this,hour,minute, DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+        final_hour = i;
+        final_minute = i1;
+        time_Show.setText("StartTime : " + final_hour + ":" + final_minute);
     }
 }
